8917c77066c390e21cb9e0011c5a84a10a3a2a98
ioeftedal <ivar.eftedal@icloud.com>
2025-11-15 15:09:44 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,140 @@
+{
+  description = "ioeftedal nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-25.05-darwin";
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-25.05";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-25.05";
+
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    nix-homebrew.url = "github:zhaofengli/nix-homebrew";
+    # Optional: Declarative tap management
+    homebrew-core = {
+      url = "github:homebrew/homebrew-core";
+      flake = false;
+    };
+    homebrew-cask = {
+      url = "github:homebrew/homebrew-cask";
+      flake = false;
+    };
+
+    theme-bobthefish = {
+      url = "github:oh-my-fish/theme-bobthefish/e3b4d4eafc23516e35f162686f08a42edf844e40";
+      flake = false;
+    };
+
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    nix-darwin,
+    home-manager,
+    nix-homebrew,
+    homebrew-core,
+    homebrew-cask,
+    theme-bobthefish,
+  }: let
+    configuration = {pkgs, ...}: {
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages = [
+        pkgs.neovim
+        pkgs.lazygit
+        pkgs.ripgrep
+        pkgs.git-get
+        pkgs.btop
+      ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Enable alternative shell support in nix-darwin.
+      programs.fish.enable = true;
+      users.users.ivareftedal = {
+        name = "ivareftedal";
+        home = "/Users/ivareftedal";
+        shell = pkgs.fish;
+      };
+
+      home-manager.users.ivareftedal = {pkgs, ...}: {
+        home.packages = [];
+        programs.bash.enable = true;
+        # The state version is required and should stay at the version you
+        # originally installed.
+        home.stateVersion = "25.05";
+
+        programs.direnv = {
+          enable = true;
+          nix-direnv.enable = true;
+
+          enableZshIntegration = true;
+          enableFishIntegration = true;
+        };
+      };
+
+      
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#Ivars-MacBook-Pro
+    darwinConfigurations."Ivars-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+      modules = [
+        configuration
+        ({...}: {
+          nix.enable = false;
+        })
+
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.backupFileExtension = "backup";
+        }
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "ivareftedal";
+
+            # Optional: Declarative tap management
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+            };
+
+            # Optional: Enable fully-declarative tap management
+            #
+            # With mutableTaps disabled, taps can no longer be added imperatively with `brew tap`.
+            mutableTaps = false;
+          };
+        }
+        # Optional: Align homebrew taps config with nix-homebrew
+        ({config, ...}: {
+          homebrew.taps = builtins.attrNames config.nix-homebrew.taps;
+        })
+      ];
+    };
+  };
+}
